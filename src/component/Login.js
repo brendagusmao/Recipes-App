395d525f07c477ed6506196c96a732cc92c90d05
Brenda Pereira Gusmão <brendapgusmao@gmail.com>
2022-10-26 19:37:14 -0300

    Estilizando pagina de login

--- src/component/Login.js
+++ src/component/Login.js
@@ -1,6 +1,8 @@
 import { useContext, useState } from 'react';
 import { Redirect } from 'react-router-dom';
 import ReceitasContext from '../context/ReceitasContext';
+import './Login.css';
+import Logo from '../images/logo.png';
 
 function Login() {
   const { userEmail, handleEmail, senha, handleSenha } = useContext(ReceitasContext);
@@ -15,39 +17,50 @@ function Login() {
     setRedirect(true);
   };
   return (
-    <>
-      <form>
-        <label htmlFor="email">
-          <input
-            type="text"
-            data-testid="email-input"
-            name="email"
-            value={ userEmail }
-            id="email"
-            onChange={ handleEmail }
-          />
-        </label>
-        <label htmlFor="senha">
-          <input
-            type="password"
-            name="senha"
-            value={ senha }
-            id="senha"
-            onChange={ handleSenha }
-            data-testid="password-input"
-          />
-        </label>
-        <button
-          type="submit"
-          data-testid="login-submit-btn"
-          onClick={ click }
-          disabled={ !(senha.length >= minNumber && emailValid) }
-        >
-          Enter
-        </button>
-      </form>
+    <div className="Login">
+      <div className="Login__container">
+        <div className="Header">
+          <img src={ Logo } alt="avatar" />
+        </div>
+        <form>
+          <label htmlFor="email">
+            <h4> Login</h4>
+            <input
+              type="text"
+              data-testid="email-input"
+              name="email"
+              value={ userEmail }
+              id="email"
+              onChange={ handleEmail }
+              className="Login__form-input"
+              placeholder=" Your e-mail"
+            />
+          </label>
+          <label htmlFor="senha">
+            <input
+              type="password"
+              name="senha"
+              value={ senha }
+              id="senha"
+              onChange={ handleSenha }
+              data-testid="password-input"
+              className="Login__form-input"
+              placeholder="Password"
+            />
+          </label>
+          <button
+            type="submit"
+            data-testid="login-submit-btn"
+            onClick={ click }
+            disabled={ !(senha.length >= minNumber && emailValid) }
+            className="Login__button"
+          >
+            Entrar
+          </button>
+        </form>
+      </div>
       {redirect && <Redirect to="/meals" />}
-    </>
+    </div>
   );
 }
 
