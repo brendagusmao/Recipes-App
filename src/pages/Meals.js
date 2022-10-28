60d2197ba634daf281b026948aff4a4793b4c58b
Gabryela Nagazawa Hayashi <gabynhp@hotmail.com>
2022-10-28 16:32:38 -0300

    oi

--- src/pages/Meals.js
+++ src/pages/Meals.js
@@ -4,27 +4,31 @@ import Header from '../component/Header';
 import ReceitasContext from '../context/ReceitasContext';
 import Card from '../component/Card';
 import Footer from '../component/Footer';
+import Recipes from './Recipes';
 
 function Meals() {
   const { receitas } = useContext(ReceitasContext);
   const route = useHistory();
-
+console.log(receitas)
   return (
     <div>
-      <Header
-        title="Meals"
-      />
-      {receitas && receitas.length === 1 ? route.push(`/meals/${receitas[0].idMeal}`)
-        : receitas && receitas.slice(0, +'12').map((recipes, id) => (
-          <Card
-            key={ id }
-            name={ recipes.strMeal }
-            dataTest={ `${id}-recipe-card` }
-            imgTest={ `${id}-card-img` }
-            imagem={ recipes.strMealThumb }
-            nameTest={ `${id}-card-name` }
-          />
-        ))}
+      <Header title="Meals" />
+      {receitas && receitas.length === 1
+        ? route.push(`/meals/${receitas[0].idMeal}`)
+        : receitas
+          && receitas
+            // .slice(0, +'12')
+            .map((recipes, id) => (
+              <Card
+                key={ id }
+                name={ recipes.strMeal }
+                dataTest={ `${id}-recipe-card` }
+                imgTest={ `${id}-card-img` }
+                imagem={ recipes.strMealThumb }
+                nameTest={ `${id}-card-name` }
+              />
+            ))}
+      <Recipes />
       <Footer />
     </div>
   );
--- src/pages/Recipes.js
+++ src/pages/Recipes.js
@@ -1,9 +1,51 @@
-import React from 'react';
-import Header from '../component/Header';
+import React, { useContext } from 'react';
+import Card from '../component/Card';
+import ReceitasContext from '../context/ReceitasContext';
 
 function Recipes() {
+  const { receitas, meal, drink } = useContext(ReceitasContext);
   return (
-    <Header title="Recipes" />
+    <div>
+      {!receitas
+        ? meal.map((recipe, id) => (
+          <Card
+            key={ id }
+            name={ recipe.strMeal }
+            dataTest={ `${id}-recipe-card` }
+            imgTest={ `${id}-card-img` }
+            imagem={ recipe.strMealThumb }
+            nameTest={ `${id}-card-name` }
+          />
+        )) : meal.map((recipes, id) => (
+          <Card
+            key={ id }
+            name={ recipes.strMeal }
+            dataTest={ `${id}-recipe-card` }
+            imgTest={ `${id}-card-img` }
+            imagem={ recipes.strMealThumb }
+            nameTest={ `${id}-card-name` }
+          />
+        ))}
+      {!receitas
+        ? drink.map((recipe, id) => (
+          <Card
+            key={ id }
+            name={ recipe.strDrink }
+            dataTest={ `${id}-recipe-card` }
+            imgTest={ `${id}-card-img` }
+            imagem={ recipe.strDrinkThumb }
+            nameTest={ `${id}-card-name` }
+          />
+        )) : drink.map((recipes, id) => (
+          <Card
+            key={ id }
+            name={ recipes.strDrink }
+            dataTest={ `${id}-recipe-card` }
+            imgTest={ `${id}-card-img` }
+            imagem={ recipes.strDrinkThumb }
+            nameTest={ `${id}-card-name` }
+          />)) }
+    </div>
   );
 }
 
