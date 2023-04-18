b439c2fac3d34244e7c8021f83970fe2e256bb95
Brenda Pereira Gusmão <brendapgusmao@gmail.com>
2023-04-18 14:45:59 -0300

    feat: css page profile

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -8,37 +8,39 @@ function Profile() {
   const { handleLogout } = useContext(ReceitasContext);
   const email = JSON.parse(localStorage.getItem('user'));
   return (
-    <>
+    <div className="containerrecipes">
       <Header title="Profile" />
-      <div className="main">
-        <p data-testid="profile-email">
-          Email:
-          {' '}
-          { email && email.email }
-        </p>
-        <a
-          href="/done-recipes"
-          data-testid="profile-done-btn"
-        >
-          Done Recipes
-        </a>
-        <a
-          href="/favorite-recipes"
-          data-testid="profile-favorite-btn"
-        >
-          Favorite Recipes
-        </a>
-        <button
-          type="button"
-          data-testid="profile-logout-btn"
-          onClick={ handleLogout }
-          className="buttomlogo"
-        >
-          Logout
-        </button>
-      </div>
       <Footer />
-    </>
+      <div className="cardDetails">
+        <section className="main">
+          <p data-testid="profile-email">
+            Email:
+            {' '}
+            { email && email.email }
+          </p>
+          <a
+            href="/done-recipes"
+            data-testid="profile-done-btn"
+          >
+            Done Recipes
+          </a>
+          <a
+            href="/favorite-recipes"
+            data-testid="profile-favorite-btn"
+          >
+            Favorite Recipes
+          </a>
+          <button
+            type="button"
+            data-testid="profile-logout-btn"
+            onClick={ handleLogout }
+            className="buttomlogo"
+          >
+            Logout
+          </button>
+        </section>
+      </div>
+    </div>
   );
 }
 
